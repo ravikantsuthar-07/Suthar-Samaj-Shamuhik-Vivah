29c3c3f2d82e8c25e9c77be872c49e13addc951d
Ravikant Suthar <ravikantsuthar4567@gmail.com>
2024-04-04 12:31:45 +0530

    Update

--- Dump20240331.sql
+++ Dump20240331.sql
@@ -35,6 +35,16 @@ CREATE TABLE `admins` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (1,'Ravikant Suthar','admin@admin.com',1234567890,'$2b$10$j.wHtv.B4B7ybBxDRfquLe9iD6qs5pMkbwDHg/lYLxIyX305g7lEu',1);
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `book`
 --
 
@@ -48,6 +58,15 @@ CREATE TABLE `book` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `contacts`
@@ -67,6 +86,15 @@ CREATE TABLE `contacts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `gallery`
 --
 
@@ -82,6 +110,16 @@ CREATE TABLE `gallery` (
   UNIQUE KEY `img_UNIQUE` (`img`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery`
+--
+
+LOCK TABLES `gallery` WRITE;
+/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+INSERT INTO `gallery` VALUES (1,2023,'1.png',0),(2,2023,'2.png',0),(3,2023,'3.png',0);
+/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `gifts`
@@ -100,6 +138,16 @@ CREATE TABLE `gifts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gifts`
+--
+
+LOCK TABLES `gifts` WRITE;
+/*!40000 ALTER TABLE `gifts` DISABLE KEYS */;
+INSERT INTO `gifts` VALUES (1,'साफा','13','3900',0,2023),(2,'सफ़ारी सूट','13','4875',0,2023),(3,'सीख','13','1300',0,2023),(4,'वधु बेस','13','16900',0,2023),(5,'21 स्टील बर्तन','13','48048',0,2023),(6,'लोहे का बक्सा','13','24180',0,2023),(7,'सोने','13','539500',0,2023),(8,'चाँदी','13','33500',0,2023),(9,'लोहे की अलमारी (6फीट)','13','123500',0,2023),(10,'प्लास्टिक की कुर्सी (4)','52','20150',0,2023),(11,'पलंग 6*4 बैक्सेडार','13','123500',0,2023);
+/*!40000 ALTER TABLE `gifts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `news`
@@ -121,6 +169,16 @@ CREATE TABLE `news` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'qwer','sdf dsajf lkds','j fklasdf jlksdajf lksdaj flkdsjafl kdsjalkf dslaf jlksadj flksjadflk jasdlkfj lksda jflsdajflksdaj lfksdjalkf jsdalf jdsa fasd fsda fasd fsda fsda fsda fs ','3.png','2024-04-02 00:22:15',1);
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `sliders`
 --
 
@@ -140,6 +198,16 @@ CREATE TABLE `sliders` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `sliders`
+--
+
+LOCK TABLES `sliders` WRITE;
+/*!40000 ALTER TABLE `sliders` DISABLE KEYS */;
+INSERT INTO `sliders` VALUES (1,' तेरहवा','2023-11-05',2023,'Slider-2023.png',1);
+/*!40000 ALTER TABLE `sliders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `teams`
 --
 
@@ -157,6 +225,16 @@ CREATE TABLE `teams` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+INSERT INTO `teams` VALUES (1,'श्री कन्हैयालाल बरड़वा','प्रणेता',8546791230,'1.png','विश्वनाथ कॉलोनी, बीकानेर',1),(2,'श्री हनुमान प्रसाद डोयल','अध्यक्ष',9821255741,'2.png','विश्वनाथ कॉलोनी, बीकानेर',1),(3,'शिवप्रकाश डोयल','मंत्री',1234567890,'3.png','नया शहर थाने के सामने, बीकानेर',1),(4,'श्री छगनलाल छड़िया','कोषाध्यक्ष',9414139391,'4.png','पुष्करणा ग्राउंड के पीछे',1);
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `weddings`
@@ -183,6 +261,20 @@ CREATE TABLE `weddings` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weddings`
+--
+
+LOCK TABLES `weddings` WRITE;
+/*!40000 ALTER TABLE `weddings` DISABLE KEYS */;
+INSERT INTO `weddings` VALUES (1,1001,2023,'महेश कुमार गेपाल','श्री ओमप्रकाश जी','स्व. श्री शंकर लाल जी','सुथारो की बड़ी गवाड़, बीकानेर',9929382549,'1001_F.png 1001_M.png','दिव्या बरड़वा','प्रेमरतन जी','गणेशदास जी','सुजानदेशर, बीकानेर',9950049948),(2,1002,2023,'अर्जुनराम मोटियार','रतनलाल जी','कानाराम जी','बंगला नगर, बीकानेर',9001165683,'1002_F.png 1002_M.png','भारती बरड़वा','प्रेमरतन जी','गणेशदास जी','सुजानदेशर, बीकानेर',9950049948),(3,1003,2023,'गणेश गेपाल','ओमप्रकाश जी','श्री शंकर लाल जी','सुथारो की बड़ी गवाड़, बीकानेर',9929382549,'1003_F.png 1003_M.png','भावना माण्डना','शिवलाल जी','छोटूराम जी','विश्वकर्मा गेट, बीकानेर',9711841247),(4,1004,2023,'द्वारकाप्रसाद जांगिड़','विनोद कुमार जी','स्व. लक्ष्मणराम जी','मुक्ता प्रसाद, बीकानेर',9602323657,'1004_F.png 1004_M.png','खुशबु बुढ़ड़','मनोज कुमार जी','नारायण जी','बंगला नगर, बीकानेर',7976759600),(5,1005,2023,'प्रकाश छाड़िया','मघाराम जी','स्व. भगवाना राम जी','नाथूसर बास, बीकानेर',8829880386,'1005_F.png 1005_M.png','शोभा खोखा','राजेन्द्र जी','रामेश्वर लाल जी','मुरलीधर व्यास कॉलोनी, बीकानेर',9772077766),(6,1006,2023,'कैलाश छाड़िया','मघाराम जी','स्व. भगवाना राम जी','नाथूसर बास, बीकानेर',8829880386,'1006_F.png 1006_M.png','सुमन खोखा','राजेन्द्र जी','रामेश्वर लाल जी','मुरलीधर व्यास कॉलोनी, बीकानेर',9772077766),(7,1007,2023,'रामु (राम) आसदेव','प्रभुदयाल जी','बुद्धाराम जी','बंगला नगर, बीकानेर',6378923305,'1007_F.png 1007_M.png','सीता नागल','हीरालाल जी','सुंदरलाल जी','मुरली मनोहर मंदिर, भिनासर, बीकानेर',9829630549),(8,1008,2023,'इंद्रचंद दुर्गेश्वर','भँवरलाल जी','किशनलाल जी','माकडो का महोला',8875552117,'1008_F.png 1008_M.png','गीता नागल','हीरालाल जी','सुंदरलाल जी','मुरली मनोहर मंदिर, भिनासर, बीकानेर',9829630549),(9,1009,2023,'गणेश बरड़वा','लालचंद जी','स्व. धूड़ाराम जी','विश्वकर्मा गेट, बीकानेर',6367694643,'1009_F.png 1009_M.png','मेगा लेखराव','मदनलाल जी','रतनलाल जी','जीवणनाथ जी बगेची, बीकानेर',8963839205),(10,1010,2023,'ब्रज मोहन लेखराव','मदनलाल जी','रतनलाल जी','जीवणनाथ जी बगेची, बीकानेर',8963839205,'1010_F.png 1010_M.png','मंजू बरड़वा','लालचंद जी','स्व. धूड़ाराम जी','विश्वकर्मा गेट, बीकानेर',6367694643),(11,1011,2023,'जसोल बामणिया','सीताराम जी','स्व. मांगीलाल जी','विश्वकर्मा कालोनी, गंगाशहर, बीकानेर',9314904595,'1011_F.png 1011_M.png','तारा कुलरिया','खींवाराम जी','स्व. अमरचंद जी','जय गणेश बिहार, बीकानेर',9928190109),(12,1012,2023,'गोविंद लदरेचा','माणकलाल जी','मोहनलाल जी','बंगला नगर, बीकानेर',8890935788,'1012_F.png 1012_M.png','नीजू धामु','मनोज जी','भीखराम जी','तिलक नगर, बीकानेर',9001345881),(13,1013,2023,'गणेशाराम मांडण','छगन लाल जी','खुमाणाराम जी','देंसलसर नौखा',9352434255,'1013_F.png 1013_M.png','निशा गेपाल','रामेश्वरलाल जी','स्व. हरखा राम, जी','विश्वकर्मा गेट, बीकानेर',9950494233);
+/*!40000 ALTER TABLE `weddings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'shamuhik'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -193,4 +285,4 @@ CREATE TABLE `weddings` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-02 14:42:28
+-- Dump completed on 2024-04-03 22:36:19
